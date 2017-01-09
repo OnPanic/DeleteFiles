d7383428abdddeb81e8c7e71fbef0caea08935df
arrase <arrase@gmail.com>
2017-01-10 00:18:50 +0100

    first commit

---
+++ app/src/main/java/org/onpanic/deletefiles/activities/PanicResponseActivity.java
@@ -0,0 +1,15 @@
+package org.onpanic.deletefiles.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PanicResponseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        finish();
+    }
+
+}
