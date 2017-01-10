18d84943e4e5ffad4b475c0c590f092ba6b6b2a9
arrase <arrase@gmail.com>
2017-01-11 00:06:26 +0100

    notification

--- app/src/main/java/org/onpanic/deletefiles/activities/PanicResponseActivity.java
+++ app/src/main/java/org/onpanic/deletefiles/activities/PanicResponseActivity.java
@@ -1,15 +1,31 @@
 package org.onpanic.deletefiles.activities;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.onpanic.deletefiles.R;
+import org.onpanic.deletefiles.notifications.TriggerNotification;
+
+import info.guardianproject.panic.PanicResponder;
 
 public class PanicResponseActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (PanicResponder.receivedTriggerFromConnectedApp(this)) {
 
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+            //  TODO
+
+            if (prefs.getBoolean(getString(R.string.pref_runned_notification), false)) {
+                TriggerNotification notification = new TriggerNotification(getApplicationContext());
+                notification.show();
+            }
+        }
 
         finish();
     }
-
 }
