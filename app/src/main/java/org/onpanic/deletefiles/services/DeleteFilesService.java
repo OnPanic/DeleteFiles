0f9e9f804f6a3f4ac92d674f01e9bd1e0de0f415
arrase <arrase@gmail.com>
2017-01-12 20:48:11 +0100

    delete all files

--- app/src/main/java/org/onpanic/deletefiles/services/DeleteFilesService.java
+++ app/src/main/java/org/onpanic/deletefiles/services/DeleteFilesService.java
@@ -15,7 +15,11 @@ import org.onpanic.deletefiles.providers.PathsProvider;
 
 import java.io.File;
 
+import static android.os.Environment.getExternalStorageDirectory;
+
 public class DeleteFilesService extends Service {
+    private SharedPreferences prefs;
+
     public DeleteFilesService() {
     }
 
@@ -28,43 +32,76 @@ public class DeleteFilesService extends Service {
     public int onStartCommand(Intent intent, int flags, final int startId) {
 
         if (intent != null && intent.getAction().equals(DeleteFilesConstants.DELETE_FILES)) {
-            final ContentResolver cr = getContentResolver();
-            String[] mProjection = new String[]{
-                    PathsProvider.Path._ID,
-                    PathsProvider.Path.PATH,
-                    PathsProvider.Path.ENABLED
-            };
 
-            final Cursor files = cr.query(PathsProvider.CONTENT_URI, mProjection, PathsProvider.Path.ENABLED + "=1", null, null);
+            prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
-            if (files == null || files.getCount() < 1) {
-                stopSelf(startId);
+            if (prefs.getBoolean(getString(R.string.pref_delete_all), false)) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        File root = getExternalStorageDirectory();
+                        for (File f : root.listFiles()) {
+                            deleteRecursive(f);
+                        }
+                        notifyRun();
+                        stopSelf(startId);
+                    }
+                }).start();
+            } else {
+                new Thread(new Runnable() {
+                    public void run() {
+                        ContentResolver cr = getContentResolver();
+                        String[] mProjection = new String[]{
+                                PathsProvider.Path._ID,
+                                PathsProvider.Path.PATH,
+                                PathsProvider.Path.ENABLED
+                        };
+
+                        Cursor files = cr.query(PathsProvider.CONTENT_URI, mProjection, PathsProvider.Path.ENABLED + "=1", null, null);
+
+                        if (files != null && files.getCount() > 0) {
+                            while (files.moveToNext()) {
+                                File current = new File(files.getString(files.getColumnIndex(PathsProvider.Path.PATH)));
+
+                                if (current.exists()) {
+                                    if (current.isDirectory()) {
+                                        deleteRecursive(current);
+                                    } else {
+                                        current.delete();
+                                    }
+                                }
+
+                                cr.delete(PathsProvider.CONTENT_URI,
+                                        PathsProvider.Path._ID + "=" + files.getInt(files.getColumnIndex(PathsProvider.Path._ID)),
+                                        null);
+                            }
+
+                            files.close();
+                            notifyRun();
+                        }
+
+                        stopSelf(startId);
+                    }
+                }).start();
             }
-
-            new Thread(new Runnable() {
-                public void run() {
-
-                    while (files.moveToNext()) {
-                        File current = new File(files.getString(files.getColumnIndex(PathsProvider.Path.PATH)));
-                        if (current.exists()) current.delete();
-                        cr.delete(PathsProvider.CONTENT_URI,
-                                PathsProvider.Path._ID + "=" + files.getInt(files.getColumnIndex(PathsProvider.Path._ID)),
-                                null);
-                    }
-
-                    files.close();
-
-                    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    if (prefs.getBoolean(getString(R.string.pref_runned_notification), false)) {
-                        TriggerNotification notification = new TriggerNotification(getApplicationContext());
-                        notification.show();
-                    }
-
-                    stopSelf(startId);
-                }
-            }).start();
         }
 
         return Service.START_STICKY;
+    }
+
+    private void deleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            for (File child : fileOrDirectory.listFiles()) {
+                deleteRecursive(child);
+            }
+        }
+
+        fileOrDirectory.delete();
+    }
+
+    private void notifyRun() {
+        if (prefs.getBoolean(getString(R.string.pref_runned_notification), false)) {
+            TriggerNotification notification = new TriggerNotification(getApplicationContext());
+            notification.show();
+        }
     }
 }
